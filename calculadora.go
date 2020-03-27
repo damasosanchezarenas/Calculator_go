51de637b80314e4c5887f7384083f6fec3cd1ca2
Dámaso <damasosanchezarenas@gmail.com>
2020-03-28 00:57:06 +0100

    Se empiezan a implementar todas las operaciones

--- calculadora.go
+++ calculadora.go
@@ -12,12 +12,31 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin) //Declaración del scanner
 	scanner.Scan()
 
-	operacion := scanner.Text()              //Coge lo que nosotros le digamos por teclado.
-	valores := strings.Split(operacion, "+") //Hacemos un split, para sacar ambos digitos. Devuelve un array de dos operadores (strings).
+	operacion := scanner.Text() //Coge lo que nosotros le digamos por teclado.
 
-	operador1, _ := strconv.Atoi(valores[0]) //Atoi devuelve dos valores.
-	operador2, _ := strconv.Atoi(valores[1])
+	operador := "p"
+	valores := strings.Split(operacion, operador) //Hacemos un split, para sacar ambos digitos. Devuelve un array de dos operadores (strings).
 
-	fmt.Println(operador1 + operador2)
+	//Atoi es un casteo para pasar string--int devuelve dos valores. Si crearamos otra variable en vez de _, tendriamos que usarla si o si
+	operador1, err1 := strconv.Atoi(valores[0])
+	operador2, err2 := strconv.Atoi(valores[1])
+
+	if err1 == nil && err2 == nil {
+
+		switch operador {
+		case "+":
+			fmt.Println(operador1 + operador2)
+		case "-":
+			fmt.Println(operador1 - operador2)
+		case "*":
+			fmt.Println(operador1 * operador2)
+		case "/":
+			fmt.Println(operador1 / operador2)
+		default:
+			fmt.Println(operador, "No esta soportado")
+		}
+	} else {
+		fmt.Println("Asegurese de introducir correctamente la operación.")
+	}
 
 }
