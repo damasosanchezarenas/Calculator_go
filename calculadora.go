3ed9eb8aa312a2f8f0342c95d4a83cba3b041add
Dámaso <damasosanchezarenas@gmail.com>
2020-03-28 13:58:01 +0100

    Se refactoriza la calculadora

--- calculadora.go
+++ calculadora.go
@@ -8,35 +8,51 @@ import (
 	"strings"
 )
 
-func main() {
+type calcu struct {
+}
+
+func (calcu) operate(entrada string, operador string) int {
+	//Hacemos un split, para sacar ambos digitos. Devuelve un array de dos operadores (strings).
+	entradaLimpia := strings.Split(entrada, operador)
+
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+
+	switch operador {
+	case "+":
+		return operador1 + operador2
+	case "-":
+		return operador1 - operador2
+	case "*":
+		return operador1 * operador2
+	case "/":
+		return operador1 / operador2
+	default:
+		fmt.Println(operador, "No esta soportado, se te devolverá un -1")
+		return -1
+	}
+}
+
+func parsear(entrada string) int {
+	//Atoi es un casteo para pasar string--int devuelve dos valores. Si crearamos otra variable en vez de _, tendriamos que usarla si o si
+	operador1, _ := strconv.Atoi(entrada)
+	return operador1
+}
+
+func leerEntrada() string {
+
 	scanner := bufio.NewScanner(os.Stdin) //Declaración del scanner
 	scanner.Scan()
-
 	operacion := scanner.Text() //Coge lo que nosotros le digamos por teclado.
 
-	operador := "p"
-	valores := strings.Split(operacion, operador) //Hacemos un split, para sacar ambos digitos. Devuelve un array de dos operadores (strings).
+	return operacion
+}
 
-	//Atoi es un casteo para pasar string--int devuelve dos valores. Si crearamos otra variable en vez de _, tendriamos que usarla si o si
-	operador1, err1 := strconv.Atoi(valores[0])
-	operador2, err2 := strconv.Atoi(valores[1])
+func main() {
 
-	if err1 == nil && err2 == nil {
+	entrada := leerEntrada()
+	operador := leerEntrada()
 
-		switch operador {
-		case "+":
-			fmt.Println(operador1 + operador2)
-		case "-":
-			fmt.Println(operador1 - operador2)
-		case "*":
-			fmt.Println(operador1 * operador2)
-		case "/":
-			fmt.Println(operador1 / operador2)
-		default:
-			fmt.Println(operador, "No esta soportado")
-		}
-	} else {
-		fmt.Println("Asegurese de introducir correctamente la operación.")
-	}
-
+	calculadora := calcu{}
+	fmt.Println(calculadora.operate(entrada, operador))
 }
